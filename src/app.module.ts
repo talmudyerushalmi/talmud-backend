9fc64f736665a10d9e48bfca47653d046ab1110e
Yaron Bar <rugggger@yahoo.co.uk>
2020-07-06 21:02:41 +0300

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppService } from './app.service';
 import { PagesModule } from './pages/pages.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ImportModule } from './import/import.module';
+import * as config from 'config';
 
+const dbConfig = config.get('db');
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/talmud?authSource=admin',
+    MongooseModule.forRoot(dbConfig.connection,
       {
-        "user": "root",
-        "pass": "root",
+        "user": dbConfig.user,
+        "pass": dbConfig.password,
         useNewUrlParser: true}),
     PagesModule,
     ImportModule
