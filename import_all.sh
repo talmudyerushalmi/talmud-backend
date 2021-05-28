ffd0746d37ec83ce69bc01a5878ca7d1ac59a692
Yaron Bar <rugggger@yahoo.co.uk>
2021-05-28 09:24:32 +0300

    import all sh

---
+++ import_all.sh
@@ -0,0 +1,3 @@
+ts-node -r tsconfig-paths/register src/console.ts import:tractates entire.txt
+ts-node -r tsconfig-paths/register src/console.ts import:settings compositions excerpts.csv/compositions_full.csv
+
