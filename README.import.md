e2d8a031f97dad98cc253937285e5510c493c5d6
Yaron Bar <rugggger@yahoo.co.uk>
2021-10-31 13:50:12 +0200

    unit tests, remove

---
+++ README.import.md
@@ -0,0 +1,13 @@
+importing sublines
+
+structure
+
+
+import sublines
+
+export NODE_ENV=production && node dist/console.js import:sublines ./nosach_261021.txt
+
+after update the excerpt selection to the new structure
+
+
+export NODE_ENV=production && node dist/console.js set:excerptSelectionSublines yevamot
--- src/pages/tractates/tractates.controller.spec.ts
+++ src/pages/tractates/tractates.controller.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PagesService } from '../pages.service';
 import { TractatesController } from './tractates.controller';
 
-describe('Tractates Controller', () => {
+fdescribe('Tractates Controller', () => {
   let controller: TractatesController;
+  let pageService: PagesService;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TractatesController],
+      providers: [{
+        provide: PagesService,
+        useValue: {
+          method1: jest.fn(),
+          method2: jest.fn(),
+        }
+      }]
     }).compile();
 
+    pageService = module.get<PagesService>(PagesService);
     controller = module.get<TractatesController>(TractatesController);
   });
 
   it('should be defined', () => {
+
+    expect(1).toBe(1);
     expect(controller).toBeDefined();
   });
 });
