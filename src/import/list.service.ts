9ce47d486ad86031c3c6d77b3a5279e17a90b25b
Yaron Bar <rugggger@yahoo.co.uk>
2021-12-20 21:03:15 +0200

    list and fix missing sources

--- src/import/list.service.ts
+++ src/import/list.service.ts
@@ -5,9 +5,13 @@ import { TractateRepository } from 'src/pages/tractate.repository';
 import { MishnaRepository } from 'src/pages/mishna.repository';
 import { CsvParser } from 'nest-csv-parser';
 import { SettingsService } from 'src/settings/settings.service';
-
+import * as fs from 'fs';
 import { SublineService } from 'src/pages/subline.service';
 import { Mishna } from 'src/pages/schemas/mishna.schema';
+
+class Entity {
+
+}
 @Console()
 @Injectable()
 export class ListService {
@@ -26,14 +30,37 @@ export class ListService {
     description: 'List empty links',
   })
   async listEmptyLinks(): Promise<void> {
+    const filename = './excerpts.csv/excerpts5_full.csv';
+    const stream = fs.createReadStream(filename);
+    const sources = await this.csvParser.parse(
+      stream,
+      Entity,
+      null,
+      null,
+      { strict: true, separator: ',' },
+    );
+    const compositions = await this.settingsService.getSettings('compositions');
     console.log('list empty links')
-    const reverse = (str)=> str;
-    const checkLinks = (m: Mishna): Promise<void> => {
+    const checkLinks = async (m: Mishna): Promise<void> => {
       const emptyLinks = m?.excerpts.filter(e=> e.type!=='NOSACH' &&  (e.source === undefined ||
          e.source.title === undefined));
       emptyLinks.forEach(emptyLink => {
-        console.log('missing: ', reverse(emptyLink.sourceLocation))
+        console.log('missing: ', emptyLink.sourceLocation)
+        const text = emptyLink.editorStateFullQuote.blocks[0].text
+        const find = sources.list.find(s => {
+         return s.excerpt === text
+        });
+        if (find) {
+          const compo = compositions.find(c => c.title === find.composition);
+          console.log(compo)
+          if (compo) {
+            emptyLink.source = compo;
+            console.log('fixed: ',emptyLink)
+          }
+        }
       })
+      m.markModified('excerpts')
+      await m.save();
       return Promise.resolve()
     }
     await this.mishnaRepo.forEachMishna(checkLinks)
