ee2b076519cec3cda4dc941f4e1db2468d9f3bbe
Yaron Bar <bar.yaron2018@gmail.com>
2022-11-27 14:53:45 +0200

    dont create synopsis on import

--- src/pages/schemas/lineMethods/createSublineFromLine.ts
+++ src/pages/schemas/lineMethods/createSublineFromLine.ts
@@ -1,36 +1,18 @@
 import { createEditorContentFromText } from "src/pages/inc/editorUtils";
-import { EditedText, SubLine, Synopsis } from "src/pages/models/line.model";
+import { SubLine } from "src/pages/models/line.model";
 
 
-function createSynopsis(text: string): Synopsis {
-
-    const editedText: EditedText = {
-        simpleText: text,
-        content: createEditorContentFromText(text)   
-
-    }
-    return {
-        text: editedText,
-        type: "direct_sources",
-        name: "כתב יד ליידן",
-        id: "leiden",
-        code: "leiden",
-        button_code: "leiden",
-    }
-
-}
 export async function createSublineFromLine(): Promise<void> {
     
     for (let i = 0; i < this.lines.length; i++) {
       const sublines: SubLine[] = []
       console.log(`${this.tractate} Line ${this.lines[i].lineNumber} subline ${i}`);
       const leidenText = getTextForSynopsis(this.lines[i].mainLine)
-      const synopsis: Synopsis = createSynopsis(leidenText)
       sublines.push({
         text: leidenText,
         index: i,
         nosach: createEditorContentFromText(leidenText),
-        synopsis: [synopsis]
+        synopsis: []
       })
       this.lines[i].sublines = sublines
     }
