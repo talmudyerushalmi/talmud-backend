bd9bf97ae1447be64cf1cadace98a1b4f20d83d2
Yaron Bar <bar.yaron2018@gmail.com>
2024-02-18 21:07:49 +0200

    payload size

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as compression from 'compression';
 import { ValidationPipe } from '@nestjs/common';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
+  app.use(json({ limit: '50mb' }));
   app.enableCors();
   app.use(compression());
 
