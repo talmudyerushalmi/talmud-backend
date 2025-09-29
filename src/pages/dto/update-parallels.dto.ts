c77285b3d6f37ac75069b2d62a78d94e52dbaefc
Asaf Corem <asaelcorem@gmail.com>
2025-09-29 11:26:06 +0300

    fix DTO bug

--- src/pages/dto/update-parallels.dto.ts
+++ src/pages/dto/update-parallels.dto.ts
@@ -1,10 +1,7 @@
-import { IsArray, ValidateNested } from 'class-validator';
-import { Type } from 'class-transformer';
+import { IsArray } from 'class-validator';
 import { InternalParallelLink } from '../models/line.model';
 
 export class UpdateParallelsDto {
   @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => InternalParallelLink)
   parallels: InternalParallelLink[];
 }
